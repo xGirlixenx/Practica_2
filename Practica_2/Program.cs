7220d512df8f01089d0c1b90986fc966c3860927
Maria del Carmen Sanchez Silva <1172701@uabc.edu.mx>
2022-09-14 17:24:22 -0700

    Agregar archivos de proyecto.

---
+++ Practica_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            int Age = 21;
+            string Name = "Maria del Carmen Sanchez";
+            string Country = "Alemania";
+
+            Console.WriteLine("Mi edad es: {0} \nMi nombre es: {1} \nY me gustaria conocer el pais de: {2}", Age, Name, Country);
+            Console.ReadKey();
+        }
+    }
+}
